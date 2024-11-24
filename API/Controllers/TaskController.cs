e8a67a3b2eedbe75d901683e18d1edeab07f0200
Bruno Silva <brunovinicius0704@gmail.com>
2024-11-24 10:31:20 -0300

    feat: adjust routes

--- API/Controllers/TaskController.cs
+++ API/Controllers/TaskController.cs
@@ -8,11 +8,11 @@ namespace API.Controllers
     public class TaskController(ITaskService service, IUserRepository userRepository) : BaseController(userRepository)
     {
         private readonly ITaskService _service = service;
-        [HttpPatch("task")]
+        [HttpPatch]
         public async Task<IActionResult> UpdateTask([FromBody] UpdateTaskRequest request) => Ok(await _service.UpdateTask(request, (await LoggedUser()).Id));
-        [HttpDelete("task/{taskId}")]
+        [HttpDelete("{taskId}")]
         public async Task<IActionResult> RemoveTask([FromRoute] Guid taskId) => Ok(await _service.RemoveTask(taskId, (await LoggedUser()).Id));
-        [HttpPost("task/add-comment")]
+        [HttpPost("add-comment")]
         public async Task<IActionResult> AddComment([FromBody] AddCommentRequest request) => Ok(await _service.AddComment(request, (await LoggedUser()).Id));
 
     }
--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -7,11 +7,11 @@ namespace API.Controllers
     public class UserController(IUserService service, IUserRepository userRepository) : BaseController(userRepository)
     {
         private readonly IUserService _service = service;
-        [HttpGet]
+        [HttpGet("project")]
         public async Task<IActionResult> ListProjects() => Ok(await _service.ListProjects((await LoggedUser()).Id));
-        [HttpPost]
+        [HttpPost("project")]
         public async Task<IActionResult> CreateProject() => Ok(await _service.CreateProject((await LoggedUser()).Id));
-        [HttpDelete("{projectId}")]
+        [HttpDelete("project/{projectId}")]
         public async Task<IActionResult> RemoveProject([FromRoute] Guid projectId) => Ok(await _service.RemoveProject(projectId, (await LoggedUser()).Id));
         [HttpGet("report")]
         public async Task<IActionResult> Report() => Ok(await _service.Report((await LoggedUser()).Id));
